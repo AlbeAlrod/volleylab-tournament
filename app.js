9fef1c0901e756fa30f498b36caec232a3a7e2ca
AlbeAlrod <albetut@gmail.com>
2026-06-04 14:51:39 +0300

    Bump Firebase to women4/men4 (vl25b_v5) to clear corrupted group data

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
-const WOMEN_REF = doc(db, "tournaments", "women3");
-const MEN_REF   = doc(db, "tournaments", "men3");
-const STORE = 'vl25b_v4';
+const WOMEN_REF = doc(db, "tournaments", "women4");
+const MEN_REF   = doc(db, "tournaments", "men4");
+const STORE = 'vl25b_v5';
 
 let firebaseReady = false;
 let applyingRemoteState = false;
